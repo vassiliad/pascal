8bd6e59a79c93191a8bfd2b5c080d0ec4f499362
Vasiliadis Vasilios <Vasiliadis.Vasilis@gmail.com>
2011-12-09 16:37:26 +0200

    constants.h re-style for documentation

--- constants.h
+++ constants.h
@@ -1,15 +1,79 @@
 #ifndef __CONSTANTS_H
 #define __CONSTANTS_H
-enum {Success, Failure};
-enum ForStepType { FT_DownTo, FT_To };
-enum StatementType { ST_If, ST_While, ST_For, ST_Call, ST_Assignment, ST_Case, ST_With };
-enum AssignmentType { AT_Expression, AT_String };
-enum VariableType { VT_Integer, VT_Real, VT_Boolean, VT_Char, VT_User, 
-      VT_Iconst, VT_Rconst, VT_Bconst , VT_Cconst};
-enum LimitType { LT_Iconst, LT_Bconst, LT_Id, LT_Cconst };
-enum EpressionType { ET_Binary, ET_Constant, ET_Not, ET_Variable, ET_Set, ET_Call };
-enum TypedefType { TT_Array, TT_List, TT_Set, TT_Record, TT_Range };
-enum BConst { False, True };
+enum 
+{
+  Success, 
+  Failure
+};
+
+enum ForStepType 
+{ 
+  FT_DownTo,
+  FT_To
+};
+
+enum StatementType 
+{
+  ST_If,
+  ST_While,
+  ST_For,
+  ST_Call, 
+  ST_Assignment,
+  ST_Case, 
+  ST_With 
+};
+
+enum AssignmentType
+{
+  AT_Expression, 
+  AT_String 
+};
+
+enum VariableType {
+  VT_Integer, 
+  VT_Real, 
+  VT_Boolean, 
+  VT_Char, 
+  VT_User, 
+  VT_Iconst, 
+  VT_Rconst, 
+  VT_Bconst,
+  VT_Cconst
+};
+
+enum LimitType 
+{ 
+  LT_Iconst,
+  LT_Bconst,
+  LT_Id, 
+  LT_Cconst
+};
+
+enum EpressionType 
+{
+  ET_Binary,
+  ET_Constant, 
+  ET_Not, 
+  ET_Variable, 
+  ET_Set, 
+  ET_Call 
+};
+
+enum TypedefType 
+{ 
+  TT_Array, 
+  TT_List, 
+  TT_Set, 
+  TT_Record, 
+  TT_Range
+};
+
+enum BConst 
+{ 
+  False, 
+  True
+};
+
 enum Operators
 {
   AddopP, AddopM,
