5c54e280f053d9b6bac9448cc770af97f5544b93
Vasiliadis Vasilis <vasiliadis.vasilis@gmail.com>
2012-01-27 22:37:36 +0200

    Added the register files

---
+++ register.c
@@ -0,0 +1,112 @@
+#include <stdlib.h>
+#include <string.h>
+#include "register.h"
+
+reg_file_t rf_saved, rf_temp,
+					 rf_fsaved, rf_ftemp;
+
+void rg_add(reg_file_t* rf, reg_t *reg)
+{
+	rf->regs = ( reg_t* ) realloc(rf->regs
+				, ( rf->size+1 ) * sizeof(reg_t) );
+	rf->regs[ rf->size++ ] = *reg;
+}
+
+void rg_init()
+{
+	// Initialize temporary variables
+	// info taken from 
+	// http://msdn.microsoft.com/en-us/library/ms253512(v=vs.80).aspx
+
+	reg_t s0 = { .name="$s0", .id=16, .in_use=0 };
+	reg_t s1 = { .name="$s1", .id=17, .in_use=0 };
+	reg_t s2 = { .name="$s2", .id=18, .in_use=0 };
+	reg_t s3 = { .name="$s3", .id=19, .in_use=0 };
+	reg_t s4 = { .name="$s4", .id=20, .in_use=0 };
+	reg_t s5 = { .name="$s5", .id=21, .in_use=0 };
+	reg_t s6 = { .name="$s6", .id=22, .in_use=0 };
+	reg_t s7 = { .name="$s7", .id=23, .in_use=0 };
+	reg_t s8 = { .name="$s8", .id=30, .in_use=0 };
+
+	reg_t t0 = { .name="$t0", .id= 8, .in_use=0 };
+	reg_t t1 = { .name="$t1", .id= 9, .in_use=0 };
+	reg_t t2 = { .name="$t2", .id=10, .in_use=0 };
+	reg_t t3 = { .name="$t3", .id=11, .in_use=0 };
+	reg_t t4 = { .name="$t4", .id=12, .in_use=0 };
+	reg_t t5 = { .name="$t5", .id=13, .in_use=0 };
+	reg_t t6 = { .name="$t6", .id=14, .in_use=0 };
+	reg_t t7 = { .name="$t7", .id=15, .in_use=0 };
+	reg_t t8 = { .name="$t8", .id=24, .in_use=0 };
+	reg_t t9 = { .name="$t9", .id=25, .in_use=0 };
+	reg_t v0 = { .name="$v0", .id= 2, .in_use=0 };
+	reg_t v1 = { .name="$v1", .id= 3, .in_use=0 };
+	reg_t a0 = { .name="$a0", .id= 4, .in_use=0 };
+	reg_t a1 = { .name="$a1", .id= 5, .in_use=0 };
+	reg_t a2 = { .name="$a2", .id= 6, .in_use=0 };
+	reg_t a3 = { .name="$a3", .id= 7, .in_use=0 };
+	
+	
+	reg_t f0  = { .name="$f0", .id=32, .in_use=0 };
+	reg_t f4  = { .name="$f4", .id=36, .in_use=0 };
+	reg_t f6  = { .name="$f6", .id=38, .in_use=0 };
+	reg_t f8  = { .name="$f8", .id=40, .in_use=0 };
+	reg_t f10 = { .name="$f10", .id=42, .in_use=0 };
+	reg_t f16 = { .name="$f16", .id=48, .in_use=0 };
+	reg_t f18 = { .name="$f18", .id=50, .in_use=0 };
+
+	reg_t f20 = { .name="$f20", .id=52, .in_use=0 };
+	reg_t f22 = { .name="$f22", .id=54, .in_use=0 };
+	reg_t f24 = { .name="$f24", .id=56, .in_use=0 };
+	reg_t f26 = { .name="$f26", .id=58, .in_use=0 };
+	reg_t f28 = { .name="$f28", .id=60, .in_use=0 };
+	reg_t f30 = { .name="$f30", .id=62, .in_use=0 };
+
+	memset(&rf_saved, 0, sizeof(reg_file_t));
+	memset(&rf_temp, 0, sizeof(reg_file_t));
+
+	memset(&rf_fsaved, 0, sizeof(reg_file_t));
+	memset(&rf_ftemp, 0, sizeof(reg_file_t));
+	
+	rg_add(&rf_saved, &s0);
+	rg_add(&rf_saved, &s1);
+	rg_add(&rf_saved, &s2);
+	rg_add(&rf_saved, &s3);
+	rg_add(&rf_saved, &s4);
+	rg_add(&rf_saved, &s5);
+	rg_add(&rf_saved, &s6);
+	rg_add(&rf_saved, &s7);
+	rg_add(&rf_saved, &s8);
+
+	rg_add(&rf_temp,&t0);
+	rg_add(&rf_temp,&t1);
+	rg_add(&rf_temp,&t2);
+	rg_add(&rf_temp,&t3);
+	rg_add(&rf_temp,&t4);
+	rg_add(&rf_temp,&t5);
+	rg_add(&rf_temp,&t6);
+	rg_add(&rf_temp,&t7);
+	rg_add(&rf_temp,&t8);
+	rg_add(&rf_temp,&t9);
+	rg_add(&rf_temp,&v0);
+	rg_add(&rf_temp,&v1);
+	rg_add(&rf_temp,&a0);
+	rg_add(&rf_temp,&a1);
+	rg_add(&rf_temp,&a2);
+	rg_add(&rf_temp,&a3);
+
+	rg_add(&rf_fsaved, &f0);
+	rg_add(&rf_fsaved, &f4);
+	rg_add(&rf_fsaved, &f6);
+	rg_add(&rf_fsaved, &f8);
+	rg_add(&rf_fsaved, &f10);
+	rg_add(&rf_fsaved, &f16);
+	rg_add(&rf_fsaved, &f18);
+	
+	rg_add(&rf_ftemp,&f20);
+	rg_add(&rf_ftemp,&f22);
+	rg_add(&rf_ftemp,&f24);
+	rg_add(&rf_ftemp,&f26);
+	rg_add(&rf_ftemp,&f28);
+	rg_add(&rf_ftemp,&f30);
+
+}
---
+++ register.h
@@ -0,0 +1,23 @@
+#ifndef __REGISTER_H
+#define __REGISTER_H
+
+typedef struct REG_FILE_T reg_file_t;
+typedef struct REG_T reg_t;
+
+struct REG_T
+{
+	char *name;
+	int id;
+	int in_use;
+};
+
+struct REG_FILE_T
+{
+	reg_t *regs;
+	int size;
+};
+
+void rg_init();
+
+
+#endif //__REGISTER_H
