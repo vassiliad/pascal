e3ea29c4081b5fe3c687a2242fa09073a6ed7bf6
Vasiliadis Vasilis <vasiliadis.vasilis@gmail.com>
2012-08-27 10:24:00 +0300

    Implemented a couple more nodes in graph.c

--- graph.c
+++ graph.c
@@ -32,9 +32,8 @@ void graph_tree(node_list_t *n, graph_t *graph) {
 
 void graph_instruction(node_t *n, graph_t *graph)
 { 
-  static int id = 0;
+  char *instr_name;
   int i = 0;
-  int me = id;
 
   for ( i=0; i<graph->num; i++ )
     if ( graph->printed[i] == n )
@@ -45,48 +44,87 @@ void graph_instruction(node_t *n, graph_t *graph)
 
   switch(n->type) {
     case NT_Iconst:
-      fprintf(graph->output, "n%d [label=\"addi %s, $0, %d\"];\n",id++, n->reg.name, n->iconst);
+      fprintf(graph->output, "n%p [label=\"addi %s, $0, %d\"];\n", n, n->reg.name, n->iconst);
     break;
+    
+
+    case NT_Load:
+      if ( n->load.address ) {
+        fprintf(graph->output, "n%p -> n%p;\n", n, n->load.address);
+        graph_instruction(n->load.address, graph);
+        fprintf(graph->output, "n%p [label=\"lw %s, %d(%s)\"];\n", n, n->reg.name, n->load.offset,
+          n->load.address->reg.name);
+      } else {
+        fprintf(graph->output, "n%p [label=\"lw %s, %d($00)\"];\n", n, n->reg.name, n->load.offset);
+      }
+
+    break;
+
     
     case NT_Add:
       if ( n->bin.right->type != NT_Iconst ) {
-        fprintf(graph->output, "n%d -> { n%d ; n%d };\n", me, id+1, id+2);
+        fprintf(graph->output, "n%p -> { n%p ; n%p };\n",n, n->bin.left, n->bin.right );
         
-        id++;
 
         graph_instruction(n->bin.left, graph);
         graph_instruction(n->bin.right, graph);
 
-        fprintf(graph->output, "n%d [label=\"add %s, %s, %s\"];\n", me, n->reg.name,
+        fprintf(graph->output, "n%p [label=\"add %s, %s, %s\"];\n", n, n->reg.name,
                   n->bin.left->reg.name, n->bin.right->reg.name);
      } else {
-        fprintf(graph->output, "n%d -> n%d\n", me, id+1);
+        fprintf(graph->output, "n%p -> n%p\n", n, n->bin.left);
        
-        id++;
 
         graph_instruction(n->bin.left, graph);
 
-        fprintf(graph->output, "n%d [label=\"addi %s, %s, %d\"];\n", me, n->reg.name,
+        fprintf(graph->output, "n%p [label=\"addi %s, %s, %d\"];\n", n, n->reg.name,
                   n->bin.left->reg.name, n->bin.right->iconst);
        
      }
     break;
+    
+    case NT_Mult:
+      fprintf(graph->output, "n%p -> { n%p ; n%p };\n",n, n->bin.left, n->bin.right );
+        
 
+      graph_instruction(n->bin.left, graph);
+      graph_instruction(n->bin.right, graph);
+
+      fprintf(graph->output, "n%p [label=\"mult %s, %s\\nmflo %s\"];\n", n, 
+                  n->bin.left->reg.name, n->bin.right->reg.name, n->reg.name);
+      
+    break;
+
+    case NT_Sub:
+      instr_name = "sub";
+      goto print_binary;
+
+    case NT_LessThan:
+      instr_name = "slt";
+    print_binary:
+      fprintf(graph->output, "n%p -> { n%p ; n%p };\n",n, n->bin.left, n->bin.right );
+      
+
+      graph_instruction(n->bin.left, graph);
+      graph_instruction(n->bin.right, graph);
+
+      fprintf(graph->output, "n%p [label=\"%s %s, %s, %s\"];\n", n, instr_name, n->reg.name,
+                n->bin.left->reg.name, n->bin.right->reg.name);
+
+    break;
 
     case NT_Store:
 
       if ( n->store.address) {
-        fprintf(graph->output, "n%d -> {n%d ; n%d};\n", me, id+1, id+2);
-        id++;
+        fprintf(graph->output, "n%p -> {n%p ; n%p};\n", n, n->store.address, n->store.data);
         graph_instruction(n->store.address, graph);
       } else {
-        fprintf(graph->output, "n%d -> n%d;\n",id, id+1);
-        id++;
+        fprintf(graph->output, "n%p -> n%p;\n",n, n->store.data);
       }
 
       graph_instruction(n->store.data, graph);
 
-      fprintf(graph->output, "n%d [label=\"sw %s, %d(0)\"];\n", me, n->store.data->reg.name, n->store.offset);
+      fprintf(graph->output, "n%p [label=\"sw %s, %d(0)\"];\n", n, n->store.data->reg.name, n->store.offset);
     break;
 
     default: {
